b5f3de6ce72773da245532e96c859514ef5ad344
artur893 <arthal893@gmail.com>
2022-06-13 00:05:30 +0200

    reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+function reverseString (wordToReverse) {
+    let array = wordToReverse.split("")
+    let reversedArray = array.reverse()
+    let reversedWord = reversedArray.join("")
+    return reversedWord
+}
 
 // Do not edit below this line
 module.exports = reverseString;
