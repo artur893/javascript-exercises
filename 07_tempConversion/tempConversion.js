86d96befdcc2dd0bc1e67ed8df58dc0f5aee5a3d
artur893 <arthal893@gmail.com>
2022-06-15 16:14:17 +0200

    Solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
+function ftoc(far) {
+  celc = (far - 32) * 5 / 9
+  fixedCelc = celc.toFixed(1)
+  numberCelc = Number(fixedCelc)
+  return numberCelc
+}
 
-};
-
-const ctof = function() {
-
-};
+function ctof(celc) {
+  far = celc * 9 / 5 + 32
+  fixedFar = far.toFixed(1)
+  numberFar = Number(fixedFar)
+  return numberFar
+}
 
 // Do not edit below this line
 module.exports = {
