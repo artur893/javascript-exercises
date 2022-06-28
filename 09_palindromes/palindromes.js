aa453953f68e0966a8ca7b7004d000f9179ef524
artur893 <arthal893@gmail.com>
2022-06-28 14:30:59 +0200

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
-};
+function palindromes(word) {
+    const lowWord = word.toLowerCase()
+    const replaced = lowWord.replace(/!/g, "")
+    const replaced2 = replaced.replace(/,/g, "")
+    const replaced3 = replaced2.replace(".", "")
+    const replaced4 = replaced3.replace(/ /g, "")
+    const splittedWord = replaced4.split("")
+    const reversed = splittedWord.reverse()
+    const equalWord = reversed.join("")
+    if (replaced4 === equalWord) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
