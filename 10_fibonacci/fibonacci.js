73ecf3fdcb894f5f920254f09121b9df955c1a06
artur893 <arthal893@gmail.com>
2022-06-28 14:56:37 +0200

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+function fibonacci(x) {
+    if (x <= 0) {
+        return ("OOPS")
+    }
+    const fibo = [1, 1]
+    for (i = 2; i < x; i++) {
+        fibo[i] = fibo[fibo.length - 1] + fibo[fibo.length - 2]
+    }
+    return fibo[x-1]
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
